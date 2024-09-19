a169e4b53fc302840aa005c7bd9e25d33a419e4a
t-ube <ube@coder.okinawa.jp>
2024-09-19 22:21:58 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
 
-export default function Home() {
+import React, { useEffect, useRef, useState } from 'react';
+import { Xumm } from 'xumm';
+import styles from './main.module.css';
+
+const CocosGame: React.FC = () => {
+  const webviewRef = useRef<HTMLIFrameElement>(null);
+  const [windowSize, setWindowSize] = useState({ width: '100vw', height: '100vh' });
+  const [bearer, setBearer] = useState<string | null>(null);
+  const [xumm, setXumm] = useState<Xumm | null>(null);
+
+  useEffect(() => {
+    const initializeXumm = async () => {
+      try {
+        const urlParams = new URLSearchParams(window.location.search);
+        const xAppToken = urlParams.get('xAppToken');
+
+        if (!xAppToken) {
+          throw new Error("No xAppToken found in URL");
+        }
+
+        const xumm = new Xumm(process.env.NEXT_PUBLIC_XUMM_API_KEY, xAppToken);
+        
+        await xumm.authorize();
+
+        const bearerToken = await xumm.environment.bearer;
+        if (bearerToken) {
+          setBearer(bearerToken);
+        } else {
+          throw new Error("Failed to obtain bearer token");
+        }
+
+        setXumm(xumm);
+
+      } catch (error) {
+        console.error('Initialization error:', error);
+      }
+    };
+
+    const handleResize = () => {
+      setWindowSize({ width: `${window.innerWidth}px`, height: `${window.innerHeight}px` });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data.type === 'FROM_COCOS') {
+        // ここでCocosからのメッセージを処理
+        console.log('Received message from Cocos:', event.data.message);
+        xumm?.payload?.create({
+          TransactionType: 'Payment',
+          Destination: 'rPJuukGFu7Awm2c2fBY8jcAndfEZQngbpD',
+          Amount: String(1)
+        }).then((payload:any) => {
+          xumm.xapp?.openSignRequest(payload)
+        })
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    initializeXumm();
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, [xumm,bearer]);
+
+  const sendMessageToCocos = (message: string) => {
+    console.log('sendMessageToCocos');
+    if (webviewRef.current) {
+      webviewRef.current.contentWindow?.postMessage({ type: 'FROM_NEXTJS', message }, '*');
+      console.log('sendMessageToCocos:OK');
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  <div id="Cocos-container" className={styles.fullscreen} style={windowSize}>
+    <iframe
+      ref={webviewRef}
+      src="https://cocos-cvx.pages.dev/"
+      className={styles.fullscreenIframe}
+      allow="autoplay; fullscreen; encrypted-media"
+    />
+    <button onClick={() => sendMessageToCocos('Hello from Next.js!')}>
+      Send Message to Cocos
+    </button>
+  </div>
   );
-}
+};
+
+export default CocosGame;
