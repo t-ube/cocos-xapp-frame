8920971b2a9e024491004b50f4b92655a6af76cd
t-ube <ube@coder.okinawa.jp>
2024-09-20 07:37:50 +0900

    add message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,17 +92,20 @@ const CocosGame: React.FC = () => {
 
   return (
   <div id="Cocos-container" className={styles.fullscreen} style={windowSize}>
-    {/*
-    <iframe
-      ref={webviewRef}
-      src="https://cocos-cvx.pages.dev/"
-      className={styles.fullscreenIframe}
-      allow="autoplay; fullscreen; encrypted-media"
-    />
-    <button onClick={() => sendMessageToCocos('Hello from Next.js!')}>
-      Send Message to Cocos
-    </button>
-    */}
+    {false && (
+      <>
+        <iframe
+          ref={webviewRef}
+          src="https://cocos-cvx.pages.dev/"
+          className={styles.fullscreenIframe}
+          allow="autoplay; fullscreen; encrypted-media"
+        />
+        <button onClick={() => sendMessageToCocos('Hello from Next.js!')}>
+          Send Message to Cocos
+        </button>
+      </>
+    )
+    }
     <button className={styles.button} onClick={() => payment()}>
       Payment
     </button>
